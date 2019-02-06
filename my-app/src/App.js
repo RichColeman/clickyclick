3ea350f4125a57de8931bd0e7d595088edd1da06
richcoleman <coleman.richardm@gmail.com>
2019-02-06 00:44:41 -0600

    ch ch changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,25 +5,6 @@ import './App.css';
 import Header from "./components/Header";
 import Tile from "./components/Tile"
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-
 function resetChecked(arr) {
   arr.forEach(player => player.checked = false )
 };
